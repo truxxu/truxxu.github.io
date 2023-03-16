5c2c465ff2bd473716e828b349eab0e1a16bd1be
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-03-15 20:54:36 -0500

    added projects section

--- src/components/desktopIcons.jsx
+++ src/components/desktopIcons.jsx
@@ -3,20 +3,20 @@ import { GlobalStyle, ThemeProvider } from "@react95/core";
 import IconBox from "./iconBox";
 import IconText from "./iconText";
 import "./styles.scss";
-import { Awfxcg321303, Explorer103 } from "@react95/icons";
+import { Awfxcg321303, Desk100 } from "@react95/icons";
 
 function Shortcuts({ openPortfolio, openCV }) {
   return (
     <div>
       <ThemeProvider>
         <GlobalStyle></GlobalStyle>
-        <IconBox className="pointer" onClick={() => openPortfolio()}>
-          <Explorer103 className="pointer" variant="32x32_4" />
-          <IconText className="pointer">Portfolio.txt</IconText>
-        </IconBox>
         <IconBox className="pointer" onClick={() => openCV()}>
           <Awfxcg321303 className="pointer" variant="32x32_4" />
           <IconText className="pointer">CV.txt</IconText>
+        </IconBox>
+        <IconBox className="pointer" onClick={() => openPortfolio()}>
+          <Desk100 className="pointer" variant="32x32_4" />
+          <IconText className="pointer">Projects</IconText>
         </IconBox>
       </ThemeProvider>
     </div>
--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -3,22 +3,6 @@ const socialMedia = [
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/janneilkka/",
   },
-  {
-    name: "Flickr",
-    url: "https://flickr.com/janneilkka",
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com/janneilkka",
-  },
-  {
-    name: "Instagram",
-    url: "https://instagram.com/janne.ilkka",
-  },
-  {
-    name: "Spotify",
-    url: "https://open.spotify.com/user/rogergregor",
-  },
 ];
 
 export default socialMedia;
